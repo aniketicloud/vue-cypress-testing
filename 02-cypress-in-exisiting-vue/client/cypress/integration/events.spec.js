f1e8a236a6b84adb225d3dcb6a38b92a0da3a503
Aniket Pandharabale <aniketicloud@gmail.com>
2021-07-25 16:22:43 +0530

    intercepting http requests

---
+++ 02-cypress-in-exisiting-vue/client/cypress/integration/events.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Event', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:8081/event', {
+      fixture: 'list-events.json',
+    }).as('list-events');
+  });
+
+  it('should list all events', () => {
+    cy.visit('/');
+    cy.wait('@list-events', { timeout: 10000 })
+  });
+});
