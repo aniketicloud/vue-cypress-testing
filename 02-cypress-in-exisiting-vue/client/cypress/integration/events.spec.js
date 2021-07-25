2df8816c94d634f3ac43facd8dbeae4da9507905
Aniket Pandharabale <aniketicloud@gmail.com>
2021-07-26 00:52:34 +0530

    accessing Vuex and Vue instance from Cypress

--- 02-cypress-in-exisiting-vue/client/cypress/integration/events.spec.js
+++ 02-cypress-in-exisiting-vue/client/cypress/integration/events.spec.js
@@ -1,14 +1,33 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 
 describe('Event', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:8081/event', {
-      fixture: 'list-events.json',
-    }).as('list-events');
+    cy.intercept('GET', 'http://localhost:8081/event',
+      {
+        fixture: 'list-events.json',
+      }).as('list-events');
+
+    cy.intercept('POST', 'http://localhost:8081/event',
+      {
+        fixture: 'create-event.json',
+      }).as('create-event')
+
   });
 
   it('should list all events', () => {
     cy.visit('/');
     cy.wait('@list-events', { timeout: 10000 });
   });
+
+  it('should create an event', () => {
+    cy.visit('/');
+    cy.window().then((win) => {
+      cy.fixture('create-event.json').then((fixture) => {
+        win.__app__.$store.dispatch('createEvent', fixture)
+      })
+      cy.wait('@create-event', { timeout: 10000 })
+    })
+  })
+
 });
--- 02-cypress-in-exisiting-vue/client/src/main.js
+++ 02-cypress-in-exisiting-vue/client/src/main.js
@@ -4,4 +4,11 @@ import store from './store'
 import router from './router'
 import 'normalize.css'
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App).use(router).use(store).mount('#app');
+
+// __app__ can be changed. 
+// Just try not to override existing property
+// (check this with typing window.__ in browser)
+if (window.Cypress) {
+  window.__app__ = app;
+}
